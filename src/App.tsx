c760dfbac43ab7b0bb2525aa2ec85114830cddaa
heydar-rzayev <heydar.rzayev@specter-automation.com>
2024-09-28 10:43:11 +0200

    feat: second approach

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,11 @@
 import { useEffect, useState } from "react";
-import { Game, GameType } from "./types";
+import { Game } from "./types";
 import { fetchGames } from "./api/service";
 import HomePage from "./pages/Homepage";
 import Navbar from "./components/Navbar";
 
 function App() {
   const [games, setGames] = useState<Game[]>([]);
-  const [showingBaseGames, setShowingBaseGames] = useState(false);
-  const [filteredGames, setFilteredGames] = useState<Game[]>([]);
-
-  const handleSearch = (searchTerm: string) => {
-    const filtered = games.filter((game) =>
-      game.name.toLowerCase().includes(searchTerm.toLowerCase())
-    );
-    setFilteredGames(filtered);
-  };
-
-  // not a big fan of doing this here but it's a small app
-  // but we would ideally have this as a global state in the store
-  const handleFilterBaseGames = () => {
-    if (showingBaseGames) {
-      setFilteredGames(games);
-      setShowingBaseGames(false);
-    } else {
-      const baseGames = games.filter((game) => game.type === GameType.BaseGame);
-      setFilteredGames(baseGames);
-      setShowingBaseGames(true);
-    }
-  };
 
   // Fetch games when the component mounts
   useEffect(() => {
@@ -35,8 +13,7 @@ function App() {
       try {
         const data = await fetchGames();
         data.sort((a, b) => a.name.localeCompare(b.name));
-        setGames(data);
-        setFilteredGames(data); // Initially show all games
+        setGames(data); // Initially show all games
       } catch (error) {
         console.error("Error loading games:", error);
       }
@@ -47,11 +24,8 @@ function App() {
 
   return (
     <div className="App">
-      <Navbar
-        onSearch={handleSearch}
-        onFilterBaseGames={handleFilterBaseGames}
-      />
-      <HomePage filteredGames={filteredGames} />
+      <Navbar />
+      <HomePage Games={games} />
     </div>
   );
 }
